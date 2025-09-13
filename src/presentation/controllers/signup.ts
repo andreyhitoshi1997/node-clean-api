2ee48eabb7e3fcc6ae416e0eae812db043c35028
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-09-13 17:52:05 -0300

    refactor: move to server Error to a refactor

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,9 @@
 import { MissingParamError } from '../errors/missing-param-error'
-import { BadRequest } from '../helpers/helper'
+import { BadRequest, ServerError } from '../helpers/http-helper'
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
 import { type Controller } from '../protocols/controller'
 import { type EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -26,10 +25,7 @@ export class SignUpController implements Controller {
         return BadRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return ServerError()
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,3 +4,8 @@ export const BadRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+
+export const ServerError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError()
+})
