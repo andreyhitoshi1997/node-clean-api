ac973cc9008f3e6b1adf35e78064db54362110fe
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-09-14 09:50:23 -0300

    refactor: move success http repsonse to a heltp method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -10,3 +10,8 @@ export const serverError = (): HttpResponse => ({
   statusCode: 500,
   body: new ServerErrorClass()
 })
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
