fa4bcb529d49e531469c9fe10ab8ecdf477ece95
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-09-15 14:02:54 -0300

    refactor: ensure email validator

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorAdapter } from "./email-validator"
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if return validator returns false ', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
