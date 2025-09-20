bf6af4758ec9a9b5341b6f3d60c6db2f94bb16b9
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-09-19 21:13:05 -0300

    test: aaaa any

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,19 @@
+import { AddAccountRepository } from "../../../../data/protocols/add-account-respository";
+import { AddAccountModel } from "../../../../domain/usecases/add-account/add-account";
+import { AccountModel } from "../../../../domain/models/account";
+import { MongoHelper } from "../helpers/mongo-helper";
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    const result = await accountCollection.insertOne(accountData);
+    const account = await accountCollection.findOne({ _id: result.insertedId });
+    const { _id, ...accountWithoutId } = account;
+    return {
+      id: _id.toHexString(),
+      name: accountWithoutId.name,
+      email: accountWithoutId.email,
+      password: accountWithoutId.password,
+    };
+  }
+}
