0b19849d95e4383cc87be6ce385a04f189023fed
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-09-24 20:34:07 -0300

    feat: commit something

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,5 @@
 import { Express } from "express";
-import { bodyParser } from "../middlewares/body-parser";
-import { cors } from "../middlewares/cors";
-import { content_type } from "../middlewares/content-type";
+import { bodyParser, cors, content_type } from "../middlewares";
 
 export default (app: Express): void => {
   app.use(bodyParser);
