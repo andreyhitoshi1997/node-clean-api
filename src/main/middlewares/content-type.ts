9937f2bbc17eed4fef3464d248f9095bb2ba26f1
andreyhitoshi1997 <andreyhitoshi1997@gmail.com>
2025-09-23 22:31:48 -0300

    chore: ensure middleware

---
+++ src/main/middlewares/content-type.ts
@@ -0,0 +1,10 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const content_type = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  res.type("json");
+  next();
+};
